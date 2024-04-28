0c9cf62100a7b1c1cfc6b7049a09b20b7bc8a9dd
Virginia <150845139+VPaigePat@users.noreply.github.com>
2024-04-28 12:38:58 -0400

    Css
    Added some css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Weather from './weather';
 import reportWebVitals from './reportWebVitals';
+import "./style.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
